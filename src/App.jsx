6fefe468b105b4a7c7077139976e26f1f0c11458
Henry Staser <h.l.staser@wustl.edu>
2025-04-10 14:45:27 -0500

    added function value button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 
   function handleClick() {
     setValue("X");
