c4c4490981ff44bfc85161d7b3e565c96a055def
Henry Staser <h.l.staser@wustl.edu>
2025-04-10 14:51:55 -0500

    added back handleclicks

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,17 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
+        <Square value={squares[0]} onSquareClick={handleClick} />
         <Square value={squares[1]} />
         <Square value={squares[2]} />
       </div>
